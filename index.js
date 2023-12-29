818cdf9b884c31690d5f0fb4653b80cb8c635fe0
CHONDO DIO <you@example.com>
2023-12-29 15:28:58 +0300

    updating

--- index.js
+++ index.js
@@ -53,3 +53,17 @@ function closeSubmenu(e) {
   document.addEventListener("click", closeSubmenu, false);
 
   
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach((entry) =>{
+    console.log(entry)
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
